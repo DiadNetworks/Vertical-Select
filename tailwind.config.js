dd005ffd29c3919770aaf88d9c6fcfb5fa2a3bad
DiadNetworks <programming@diadnet.com>
2025-05-14 14:47:08 -0400

    Added dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'media', // Enable dark mode based on user's system preference
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
