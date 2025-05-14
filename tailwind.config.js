84a90f19a3591b2c67467158040fa86a3989b350
DiadNetworks <programming@diadnet.com>
2025-05-14 14:48:12 -0400

    Revert "Added dark mode"
    This reverts commit dd005ffd29c3919770aaf88d9c6fcfb5fa2a3bad.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'media', // Enable dark mode based on user's system preference
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
